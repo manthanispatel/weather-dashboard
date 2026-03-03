385e99211ca894fffb719b5285fafbef779ae6d3
Manthan Patel <manthanp1104@gmail.com>
2026-03-02 20:27:35 -0800

    Build Python FastAPI backend, connect OpenWeather API, and setup .gitignore for security

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,45 @@
+import os
+import requests
 from fastapi import FastAPI
+from dotenv import load_dotenv
 
-# Initialize the FastAPI app
+# 1. Unlock the .env safe
+load_dotenv()
+
 app = FastAPI()
 
-# Create our first "Route" (or endpoint)
+# 2. Grab the API key securely from the safe
+API_KEY = os.getenv("API_KEY")
+
+# ADD THIS LINE:
+print(f"DEBUG: My API Key is currently -> {API_KEY}")
+
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Weather API Engine!"}
+
+@app.get("/api/weather/{city}")
+def get_weather(city: str):
+
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    # Check if the city was found
+    if response.status_code != 200:
+        return {"error": "City not found"}
+
+    
+
+    data = response.json()
+
+    #Parse the relevant weather data
+    clean_data = {
+        "city": data["name"],
+        "temperature": data["main"]["temp"],
+        "description": data["weather"][0]["description"].title(), 
+        "humidity": data["main"]["humidity"],
+        "icon": data["weather"][0]["icon"] 
+    }
+
+    return clean_data
